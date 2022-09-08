82b6a204928a853a8bdd93afa463ccb860a628aa
stewie-gil <stewie-gil>
2022-09-08 05:10:02 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
 *main- Entry
 *
@@ -5,6 +7,6 @@
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-return (0)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

8dc7fc85e43ac6d1a9f8b9bc24e476d6b1c147bb
stewie-gil <stewie-gil>
2022-09-09 16:59:01 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Prints upper and lowercase alphabet
+* 
+*Return : Always 0
+*/
+int main(void)
+{
+  char lower;
+  char upper;
+  for (lower = 'a', lower <= 'z', lower

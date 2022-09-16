94c93ca7026f75171915dd449ab9b7b08893f35f
stewie-gil <stewie-gil>
2022-09-16 06:26:45 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "1-main.c"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return: Always (Success)
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+

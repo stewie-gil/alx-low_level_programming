09fd02f6aaa15a333eb983650bd43515ea97c974
stewie-gil <stewie-gil>
2022-09-30 10:18:34 -0700

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - return name of the programe
+ * @agrc: argument count
+ * @agrv: argument vector
+ * Return: always zero
+ */
+int main(int agrc, char *agrv[])
+{
+(void)agrc;
+printf("%s\n", agrv[0]);
+return (0);
+}

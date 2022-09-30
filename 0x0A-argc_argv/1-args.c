6cc78dc18560624fa1b7876418ae50aa4109742f
stewie-gil <stewie-gil>
2022-09-30 10:53:46 -0700

    m

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - printing the number of argvs
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}

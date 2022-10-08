4e1f7eb4230ff73407233e0d4bc2ce8ed13fbdd7
stewie-gil <stewie-gil>
2022-10-07 23:25:47 -0700

    m

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory
+ *
+ * @b: the number
+ *
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+unsigned int *i;
+i = malloc(b);
+if (i == NULL)
+exit(98);
+return (i);
+}

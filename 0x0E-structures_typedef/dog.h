e7d7ac8a6fbd5a3a8ab86da5ac266c82c54cf1f2
stewie-gil <stewie-gil>
2022-10-13 07:27:56 -0700

    m

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - just a dog
+ * @name: name of the dog
+ * @owner: who owns it
+ * @age: how old
+ */
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+#endif

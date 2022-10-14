dd6b4e7254e4436c7f84ca779035dd8f0dbd0c08
stewie-gil <stewie-gil>
2022-10-14 12:51:17 -0700

    m

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - struct dog
+ * @d: the structure
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+if (d)
+{
+if (d->name)
+printf("Name: %s\n", d->name);
+else
+printf("Name: (nil)\n");
+printf("Age: %.6f\n", d->age);
+if (d->owner)
+printf("Owner: %s\n", d->owner);
+else
+printf("Owner: (nil)\n");
+}
+else
+return;
+}

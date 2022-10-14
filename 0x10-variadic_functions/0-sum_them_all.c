b9750b992db79b81018aab6dd1e7b05d7064bf94
stewie-gil <stewie-gil>
2022-10-14 13:31:55 -0700

    sum of all parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sum of parameters
+ * @n: arguments
+ * Return: the sum of parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list valist;
+unsigned int sum = 0, i;
+if (n == 0)
+return (0);
+va_start(valist, n);
+for (i = 0; i < n; i++)
+sum += va_arg(valist, int);
+va_end(valist);
+return (sum);
+}

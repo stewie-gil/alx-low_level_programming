fe919edb061c7583f3be81d78583bbcb4b35998e
stewie-gil <stewie-gil>
2022-10-14 12:59:54 -0700

    added struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,14 @@ char *name;
 char *owner;
 float age;
 };
+/**
+ * typedef dog_t - new name for struct dog
+ */
+
+typedef struct dog dog_t;
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

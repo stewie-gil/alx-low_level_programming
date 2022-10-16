b3beba4dedf3fbf8a498467933dce6a099d18c5e
stewie-gil <stewie-gil>
2022-10-16 08:51:09 -0700

    return

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+]0;276;0c#include "function_pointers.h"
 /**
  * print_name - print a name
  * @name: name
@@ -8,6 +8,6 @@
 void print_name(char *name, void(*f)(char *))
 {
 if (!name || !f)
-f(name);
 return;
+f(name);
 }

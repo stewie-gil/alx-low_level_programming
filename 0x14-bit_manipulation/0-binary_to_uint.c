70542c344cc34ce1a0b1174633b91953ee31118d
stewie-gil <stewie-gil>
2022-11-04 02:54:46 -0700

    m

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
  * binary_to_int - converts binary to interger
- *
- * Return: always zero 
+ * @res: unasigned int
+ * Return: always zero
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-  unsigned res =0;
-  if (!b)
-    return (0);
-  while (*b)
-    {
-
-      if(*b != '0' && *b != '1')
-	return (0);
-      res<<=1;
-      if (*b & 1)
-	res +=1;
-      b++;
-    }
-  return res;
-
-
-
+unsigned int res = 0;
+if (!b)
+return (0);
+while (*b)
+{
+if (*b != '0' && *b != '1')
+{
+return (0);
+}
+res <<= 1;
+if (*b & 1);
+{
+res += 1;
+b++;
+}
+}
+return (res);
 }

35e92810bacb7ad3acf8cfd86eded6f376c159e5
stewie-gil <stewie-gil>
2022-11-04 08:38:56 -0700

    m

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * rev_string - reverses a string
  * @s: holi
@@ -9,21 +9,14 @@ void rev_string(char *s)
 {
 int i;
 int j;
-int temp;
-int length;
-while (s[i] != 0)
+int k;
+ k=0;
+ i=0;
+ 
+while (s[k] != '\0')
 {
 i++;
 }
-length = i;
-i = 0;
-j = length - 1;
-while (i < j)
-{
-temp = s[i];
-s[i] = s[j];
-s[j] = temp;
-i++;
-j--;
-}
+for (j = i; j > 0; j--)
+  printf("%c",s[j]);
 }

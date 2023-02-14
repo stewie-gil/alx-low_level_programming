576794ffb1cb5760e63b744e84173db8a4e2a401
stewie-gil <gilliganstewie7@gmail.com>
2023-02-14 13:29:51 -0800

    h

---
+++ 0x09-static_libraries/alx-low_level_programming/isdigit.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+*
+*  * _isdigit - Write a function that checks for a digit 0-9.
+*
+*   * @c: parameter for function.
+*
+*    * Return: Always 0.
+*
+*     */
+
+int _isdigit(int c)
+
+{
+
+if (c >= '0' && c <= '9')
+
+{
+
+return (1);
+
+}
+
+else
+
+{
+
+return (0);
+
+}
+
+
+
+}
---
+++ 0x18-dynamic_libraries/.#cop.h
@@ -0,0 +1,1 @@
+root@ee7c6bcd79ee.5921
